56ca6c757d5e4ea4fe426f7607a498b3c446a3ed
Cemalettin Altıntaş <93022253+cemalettinaltintas@users.noreply.github.com>
2022-12-04 18:18:48 +0300

    alert dialog updated

--- AlertDialog/app/src/main/java/com/cemalettinaltintas/alertdialog/MainActivity.java
+++ AlertDialog/app/src/main/java/com/cemalettinaltintas/alertdialog/MainActivity.java
@@ -2,7 +2,12 @@ package com.cemalettinaltintas.alertdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void save(View view){
+        AlertDialog.Builder alert=new AlertDialog.Builder(this);
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //save
+                Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"Not Saved",Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.show();
     }
 }
