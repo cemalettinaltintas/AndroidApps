ceda04550aadf2b198a0071b1f280b5ecba83c25
Cemalettin Altıntaş <93022253+cemalettinaltintas@users.noreply.github.com>
2022-12-09 09:12:55 +0300

    landmark finished

--- LandmarkBook/app/src/main/java/com/cemalettinaltintas/landmarkbook/DetailsActivity.java
+++ LandmarkBook/app/src/main/java/com/cemalettinaltintas/landmarkbook/DetailsActivity.java
@@ -2,13 +2,28 @@ package com.cemalettinaltintas.landmarkbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.cemalettinaltintas.landmarkbook.databinding.ActivityDetailsBinding;
 
 public class DetailsActivity extends AppCompatActivity {
-
+    private ActivityDetailsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Intent intent=getIntent();
+
+        Landmark selectedLandmark=(Landmark) intent.getSerializableExtra("landmark");
+        binding.nameText.setText(selectedLandmark.name);
+        binding.countryText.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
+
     }
 }
