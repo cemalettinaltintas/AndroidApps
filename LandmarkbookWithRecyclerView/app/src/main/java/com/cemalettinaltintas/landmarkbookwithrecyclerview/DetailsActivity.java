92f5057f584c105270247a7b1941a55051cb113f
Cemalettin Altıntaş <93022253+cemalettinaltintas@users.noreply.github.com>
2022-12-09 11:14:22 +0300

    landmarkbook with recycler view finished

--- LandmarkbookWithRecyclerView/app/src/main/java/com/cemalettinaltintas/landmarkbookwithrecyclerview/DetailsActivity.java
+++ LandmarkbookWithRecyclerView/app/src/main/java/com/cemalettinaltintas/landmarkbookwithrecyclerview/DetailsActivity.java
@@ -2,13 +2,26 @@ package com.cemalettinaltintas.landmarkbookwithrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.cemalettinaltintas.landmarkbookwithrecyclerview.databinding.ActivityDetailsBinding;
 
 public class DetailsActivity extends AppCompatActivity {
-
+    private ActivityDetailsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        binding=ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+        Intent intent=getIntent();
+        Landmark selectedLandmark=(Landmark) intent.getSerializableExtra("landmark");
+
+        binding.nameText.setText(selectedLandmark.name);
+        binding.countryText.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
+
     }
 }
--- LandmarkbookWithRecyclerView/app/src/main/java/com/cemalettinaltintas/landmarkbookwithrecyclerview/MainActivity.java
+++ LandmarkbookWithRecyclerView/app/src/main/java/com/cemalettinaltintas/landmarkbookwithrecyclerview/MainActivity.java
@@ -1,20 +1,43 @@
 package com.cemalettinaltintas.landmarkbookwithrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.cemalettinaltintas.landmarkbookwithrecyclerview.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    ArrayList<Landmark> landmarkArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        landmarkArrayList=new ArrayList<>();
+
+        Landmark pisa=new Landmark("Pisa","Italy",R.drawable.pisa);
+        Landmark colloseum=new Landmark("Colloseum","France",R.drawable.colloseum);
+        Landmark london=new Landmark("London Bridge","UK",R.drawable.london);
+        Landmark eiffel=new Landmark("Eiffel Tower","France",R.drawable.eiffel);
+
+        landmarkArrayList.add(pisa);
+        landmarkArrayList.add(colloseum);
+        landmarkArrayList.add(eiffel);
+        landmarkArrayList.add(london);
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LandmarkAdapter landmarkAdapter=new LandmarkAdapter(landmarkArrayList);
+
+        binding.recyclerView.setAdapter(landmarkAdapter);
+
+
     }
 
 }
